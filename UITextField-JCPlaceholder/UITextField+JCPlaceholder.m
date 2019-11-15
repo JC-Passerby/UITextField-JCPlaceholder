c326e7117d3fcff093fd1b492b3cd7fbaa5a3bd3
liujc <506551555@qq.com>
2019-11-15 20:16:04 +0800

    no message

--- UITextField-JCPlaceholder/UITextField+JCPlaceholder.m
+++ UITextField-JCPlaceholder/UITextField+JCPlaceholder.m
@@ -10,25 +10,27 @@
 #import <objc/runtime.h>
 
 @implementation UITextField (JCPlaceholder)
-+ (void)load{
+
++ (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        Class selfClass = [self class];
-        Method systemMethod = class_getInstanceMethod(selfClass, @selector(setPlaceholder:));
-        Method customizeMethod = class_getInstanceMethod(selfClass, @selector(jc_setPlaceholder:));
-        
+        Class class = [self class];
+        SEL originalSelector = @selector(setPlaceholder:);
+        SEL swizzledSelector = @selector(jc_setPlaceholder:);
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
         BOOL didAddMethod =
             class_addMethod(class,
                 originalSelector,
-                method_getImplementation(customizeMethod),
-                method_getTypeEncoding(customizeMethod));
+                method_getImplementation(swizzledMethod),
+                method_getTypeEncoding(swizzledMethod));
         if (didAddMethod) {
             class_replaceMethod(class,
                 swizzledSelector,
-                method_getImplementation(systemMethod),
-                method_getTypeEncoding(systemMethod));
+                method_getImplementation(originalMethod),
+                method_getTypeEncoding(originalMethod));
         } else {
-            method_exchangeImplementations(systemMethod, customizeMethod);
+            method_exchangeImplementations(originalMethod, swizzledMethod);
         }
     });
 }
@@ -36,7 +38,7 @@
 -(void)jc_setPlaceholder:(NSString *)placeholder{
     NSMutableAttributedString *placeholderAtbString = [[NSMutableAttributedString alloc] initWithString:placeholder];
     [placeholderAtbString addAttribute:NSForegroundColorAttributeName
-                                 value:VALUECORLOR
+                                 value:[UIColor blackColor]
                                  range:NSMakeRange(0, placeholder.length)];
     self.attributedPlaceholder = placeholderAtbString;
     [self jc_setPlaceholder:placeholder];
